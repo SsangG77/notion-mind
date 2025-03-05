d3943a2f52fefba28acd9a25e268e348eefd8e72
sangjin <sangjincha719@gmail.com>
2025-03-05 20:57:24 +0900

    Initial Commit

---
+++ notion-mind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notion-mind
+//
+//  Created by 차상진 on 3/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
